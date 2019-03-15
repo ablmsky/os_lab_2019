3d1267f0ce4c2c27e570a79493d3865fa19ee8b8
Anton Ablamskiy <ablamskiy98@gmail.com>
2019-03-15 13:53:12 +0000

    lab2 done

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
-
+#include "swap.h"
+#include "stdio.h"
 void RevertString(char *str)
 {
 	// your code here
+	char* endPtr = str;
+	while(*(endPtr+1) != '\0')
+	   endPtr++;
+	   
+	int bound =( endPtr-str)%2 == 0 ? (endPtr-str)/2 : (endPtr-str)/2+1;
+	for(int i = 0;i< bound ; i++){
+        Swap(endPtr-i,str+i);	   
+	}
 }
 
